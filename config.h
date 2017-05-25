d0a505d7c9859840c17568d7e7748e261c44a897
superpingu <arnaudbonetti@me.com>
2017-05-26 00:40:24 +0200

    config

--- config.h
+++ config.h
@@ -6,6 +6,7 @@
 #define YELLOW_SENSOR_MIDDLE 5
 
 #define MAX_GAME_LENGTH_MILLIS 90000
-#define FINAL_ACTION_DELAY_MILLIS 82000
 
-#define TEST_OR_GAME_PIN
+#define TEST_OR_GAME_PIN 26
+
+#define MAIN_PROGRAM "/home/pi/Stardust/prod/main_program/main"
